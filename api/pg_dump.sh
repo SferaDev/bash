56a2ac1a8a47f2f9b3922482a655997b715ded6a
Alexis Rico <sferadev@gmail.com>
2025-02-11 18:43:37 +0100

    Test
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- api/pg_dump.sh
+++ api/pg_dump.sh
@@ -1,3 +1,5 @@
 handler() {
-    ../bin/pg_dump --version
+    wget https://github.com/SferaDev/bash/raw/0875b0b7da1c9c81df535383bd0f4247e915ef18/bin/pg_dump
+    chmod +x pg_dump
+    ./pg_dump --version
 }
